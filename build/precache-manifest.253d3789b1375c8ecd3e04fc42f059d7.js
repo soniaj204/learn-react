3ba274b940f2199367c92a6ce8c88149f5aeb888
Sonia Jain <baps@bapss-MacBook-Pro-2.local>
2019-07-23 18:35:33 +0530

    section 7

---
+++ build/precache-manifest.253d3789b1375c8ecd3e04fc42f059d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9f5228b9a2071ec05d9d556bc92374",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79425b8296bea006b7d4",
+    "url": "/static/css/main.37a0e1f8.chunk.css"
+  },
+  {
+    "revision": "03310fa034cae3128540",
+    "url": "/static/js/2.41d85438.chunk.js"
+  },
+  {
+    "revision": "79425b8296bea006b7d4",
+    "url": "/static/js/main.7c9c5182.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
