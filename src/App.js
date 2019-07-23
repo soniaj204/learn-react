45733361894efd1fe220ec397ac2464f6e163464
Sonia Jain <baps@bapss-MacBook-Pro-2.local>
2019-07-23 18:41:31 +0530

    delete unused files

--- src/App.js
+++ src/App.js
@@ -1,90 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import Person from "./Person/Person";
-
 class App extends Component {
-  state = {
-    persons: [
-      {
-        id: "1",
-        name: "Sonia",
-        age: 1
-      },
-      {
-        id: "2",
-        name: "SoniaJ",
-        age: 2
-      },
-      {
-        id: "3",
-        name: "SoniaJa",
-        age: 3
-      }
-    ],
-    other: "other value",
-    showPersons: false
-  };
-
-  nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
-    });
-
-    const person = {
-      ...this.state.persons[personIndex]
-    };
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-
-    persons[personIndex] = person;
-
-    this.setState({
-      persons: persons
-    });
-  };
-
-  deletePersonHandler = personIndex => {
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({ persons: persons });
-  };
-
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({
-      showPersons: !doesShow
-    });
-  };
-
   render() {
-    let persons = null;
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={event => this.nameChangedHandler(event, person.id)}
-              />
-            );
-          })}
-        </div>
-      );
-    }
-
     return (
       <div className="App">
         <h1>React Project</h1>
         <p>This is really working!</p>
-        <button onClick={this.togglePersonsHandler}>Switch Name</button>
-        {persons}
       </div>
     );
   }
